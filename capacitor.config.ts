3c24790aac0c4c53de2f12846f2f2dbf394c11e3
bibhasingh <bibhaanil@gmail.com>
2023-02-28 22:03:21 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quiz-proj-indv',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
